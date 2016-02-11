e85755100aea193219fabcf20a8b33a0cf2e6872
MarcinMach <marcinmach.bb@gmail.com>
2016-02-11 15:07:57 +0100

    ints

--- Services/App_Start/RouteConfig.cs
+++ Services/App_Start/RouteConfig.cs
@@ -14,6 +14,11 @@ namespace Services
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
             routes.MapRoute(
+                name: "Customers",
+                url: "{controller}/{action}/{id}",
+                defaults: new { controller = "Customers", action = "Index", id = UrlParameter.Optional });
+
+            routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
